4193de56d3deafcc3d32337ee86cf8a3259d724e
Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
2022-10-13 13:37:49 +0900

    Merge setup commands to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+python -m pip install pipenv-shebang
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
+echo "y" | pipenv install --skip-lock
 
 if [ -e "scripts/yolact/weights/yolact_plus_resnet50_54_800000.pth" ]; then
     echo "Yolact weights already downloaded"
